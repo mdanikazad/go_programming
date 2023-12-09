071c4b4f7bb3580937b2681934fa82a262faa33f
Onik Azad <62051809+mdanikazad@users.noreply.github.com>
2023-12-09 14:32:19 +0800

    Update var.go

--- var.go
+++ var.go
@@ -1,22 +1,14 @@
-//basics of variable
-
 package main
 
 import "fmt"
 
 func main() {
 
-	// var declaration
-	//fmt.Println("Hello World")
-	var fullName, country string
-	var age int
-	var gpa float32
-
-	// var initialization
-	fullName = "Onik Azad"
-	country = "Bangladesh"
-	age = 32
-	gpa = 5
+	// var initialization using short assignment statement.
+	fullName := "Onik Azad"
+	country := "Bangladesh"
+	age := 32
+	gpa := 5
 
 	fmt.Println(fullName, "is a student")
 	fmt.Println(fullName, "is ", age, "years old")
